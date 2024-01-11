2ddc3fc39504ed94c612c1c64b47082577097b4e
Evan Klein <eklein217@gmail.com>
2024-01-11 13:15:38 -0500

    Change name, play around with the test structure

--- tests/multiplication-test-1.spec.ts
+++ tests/multiplication-test-1.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Multiply 7x9 correctly', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await page.getByRole('button', { name: '7' }).click();
   await page.getByRole('button', { name: 'x' }).click();
