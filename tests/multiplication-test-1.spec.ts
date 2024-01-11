f3278416efa6dbbe083eb8960bdc47fd179ce2d7
Evan Klein <eklein217@gmail.com>
2024-01-11 13:11:09 -0500

    Add playwright and a multiplication test

---
+++ tests/multiplication-test-1.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('button', { name: '7' }).click();
+  await page.getByRole('button', { name: 'x' }).click();
+  await page.getByRole('button', { name: '9' }).click();
+  await page.getByRole('button', { name: '=' }).click();
+  await expect(page.locator('#root')).toContainText('63');
+});
